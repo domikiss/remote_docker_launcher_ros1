d31827c8d379ab8f636f7fe4d91a0663dec88e3b
Domokos Kiss <domikiss@aut.bme.hu>
2024-11-04 22:55:15 +0100

    Removing unnecessary entrypoint.sh from the example Docker image

--- docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Basic entrypoint for ROS / Catkin Docker containers
- 
-# Source ROS
-source /opt/ros/${ROS_DISTRO}/setup.bash
- 
-# Execute the command passed into this entrypoint
-exec "$@"
